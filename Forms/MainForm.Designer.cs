00e9e481ae2308d78e205615cd1474d3d4050d90
fazli65 <mohammad_fazli65@yahoo.com>
2020-03-31 02:34:35 +0430

    create

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SGRSalary.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.تعاریفپایهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ورودفایلToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.شرکتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.پرسنلToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.نوعبازاریابیToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.وضعیتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.بانکToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.فاصلهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.صنفToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.مسئولمنطقهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.نامتجهیزاتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.نوعتکلیفیToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تعاریفپایهToolStripMenuItem,
+            this.ورودفایلToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // تعاریفپایهToolStripMenuItem
+            // 
+            this.تعاریفپایهToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.شرکتToolStripMenuItem,
+            this.پرسنلToolStripMenuItem,
+            this.نوعبازاریابیToolStripMenuItem,
+            this.وضعیتToolStripMenuItem,
+            this.بانکToolStripMenuItem,
+            this.فاصلهToolStripMenuItem,
+            this.صنفToolStripMenuItem,
+            this.مسئولمنطقهToolStripMenuItem,
+            this.نامتجهیزاتToolStripMenuItem,
+            this.نوعتکلیفیToolStripMenuItem});
+            this.تعاریفپایهToolStripMenuItem.Name = "تعاریفپایهToolStripMenuItem";
+            this.تعاریفپایهToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.تعاریفپایهToolStripMenuItem.Text = "تعاریف پایه";
+            // 
+            // ورودفایلToolStripMenuItem
+            // 
+            this.ورودفایلToolStripMenuItem.Name = "ورودفایلToolStripMenuItem";
+            this.ورودفایلToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.ورودفایلToolStripMenuItem.Text = "ورود فایل";
+            // 
+            // شرکتToolStripMenuItem
+            // 
+            this.شرکتToolStripMenuItem.Name = "شرکتToolStripMenuItem";
+            this.شرکتToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.شرکتToolStripMenuItem.Text = "شرکت";
+            // 
+            // پرسنلToolStripMenuItem
+            // 
+            this.پرسنلToolStripMenuItem.Name = "پرسنلToolStripMenuItem";
+            this.پرسنلToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.پرسنلToolStripMenuItem.Text = "پرسنل";
+            // 
+            // نوعبازاریابیToolStripMenuItem
+            // 
+            this.نوعبازاریابیToolStripMenuItem.Name = "نوعبازاریابیToolStripMenuItem";
+            this.نوعبازاریابیToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.نوعبازاریابیToolStripMenuItem.Text = "نوع بازاریابی";
+            // 
+            // وضعیتToolStripMenuItem
+            // 
+            this.وضعیتToolStripMenuItem.Name = "وضعیتToolStripMenuItem";
+            this.وضعیتToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.وضعیتToolStripMenuItem.Text = "وضعیت";
+            // 
+            // بانکToolStripMenuItem
+            // 
+            this.بانکToolStripMenuItem.Name = "بانکToolStripMenuItem";
+            this.بانکToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.بانکToolStripMenuItem.Text = "بانک";
+            // 
+            // فاصلهToolStripMenuItem
+            // 
+            this.فاصلهToolStripMenuItem.Name = "فاصلهToolStripMenuItem";
+            this.فاصلهToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.فاصلهToolStripMenuItem.Text = "مقیاس محدوده";
+            // 
+            // صنفToolStripMenuItem
+            // 
+            this.صنفToolStripMenuItem.Name = "صنفToolStripMenuItem";
+            this.صنفToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.صنفToolStripMenuItem.Text = "صنف";
+            // 
+            // مسئولمنطقهToolStripMenuItem
+            // 
+            this.مسئولمنطقهToolStripMenuItem.Name = "مسئولمنطقهToolStripMenuItem";
+            this.مسئولمنطقهToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.مسئولمنطقهToolStripMenuItem.Text = "مسئول منطقه";
+            // 
+            // نامتجهیزاتToolStripMenuItem
+            // 
+            this.نامتجهیزاتToolStripMenuItem.Name = "نامتجهیزاتToolStripMenuItem";
+            this.نامتجهیزاتToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.نامتجهیزاتToolStripMenuItem.Text = "نام تجهیزات";
+            // 
+            // نوعتکلیفیToolStripMenuItem
+            // 
+            this.نوعتکلیفیToolStripMenuItem.Name = "نوعتکلیفیToolStripMenuItem";
+            this.نوعتکلیفیToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.نوعتکلیفیToolStripMenuItem.Text = "نوع تکلیفی";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem تعاریفپایهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem شرکتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem پرسنلToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem نوعبازاریابیToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem وضعیتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem بانکToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem فاصلهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem صنفToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem مسئولمنطقهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem نامتجهیزاتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem نوعتکلیفیToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ورودفایلToolStripMenuItem;
+    }
+}

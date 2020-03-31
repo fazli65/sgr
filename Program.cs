e4eba43bab4d2b85239184b0c751d672b2907cb9
mahdavian <mahdavian@samiansoft.com>
2020-03-31 19:05:48 +0430

    add context

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SGRSalary.Forms;
 
 namespace SGRSalary
 {
@@ -16,7 +17,7 @@ namespace SGRSalary
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }

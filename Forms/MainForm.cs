01c934ff1de88782ad0a76f1a3f2a2186447d145
fazli65 <mohammad_fazli65@yahoo.com>
2020-03-31 13:35:03 +0430

    .

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -16,5 +16,20 @@ namespace SGRSalary.Forms
         {
             InitializeComponent();
         }
+
+        private void فیشحقوقیToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void شرکتToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
